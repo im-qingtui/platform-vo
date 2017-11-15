6c026894c87e1f0a825bd60e2519b570cb238cb0
yiya <yuanye.li@cisdi.com.cn>
2017-11-15 19:43:51 +0800

    2.0.2 web组件添加下划线转驼峰过滤器

---
+++ platform-web/src/main/java/im/qingtui/platform/web/wrapper/ParameterRequestWrapper.java
@@ -0,0 +1,94 @@
+package im.qingtui.platform.web.wrapper;
+
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Vector;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * 自定义HTTP请求包装类，用于将下划线风格的参数包装成驼峰风格参数
+ *
+ * @author yiya
+ */
+public class ParameterRequestWrapper extends HttpServletRequestWrapper {
+
+    private Map<String, String[]> params;
+
+    public ParameterRequestWrapper(HttpServletRequest request,
+        Map<String, String[]> newParams) {
+        super(request);
+
+        this.params = newParams;
+    }
+
+    @Override
+    public String getParameter(String name) {
+        String result = "";
+
+        Object v = params.get(name);
+        if (v == null) {
+            result = null;
+        } else if (v instanceof String[]) {
+            String[] strArr = (String[]) v;
+            if (strArr.length > 0) {
+                result = strArr[0];
+            } else {
+                result = null;
+            }
+        } else if (v instanceof String) {
+            result = (String) v;
+        } else {
+            result = v.toString();
+        }
+
+        return result;
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        return params;
+    }
+
+    @Override
+    public Enumeration getParameterNames() {
+        return new Vector(params.keySet()).elements();
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] result = null;
+
+        Object v = params.get(name);
+        if (v == null) {
+            result = null;
+        } else if (v instanceof String[]) {
+            result = (String[]) v;
+        } else if (v instanceof String) {
+            result = new String[]{(String) v};
+        } else {
+            result = new String[]{v.toString()};
+        }
+
+        return result;
+    }
+
+    public void addAllParameters(Map<String , Object>otherParams) {//增加多个参数
+        for(Map.Entry<String , Object>entry : otherParams.entrySet()) {
+            addParameter(entry.getKey() , entry.getValue());
+        }
+    }
+
+    public void addParameter(String name , Object value) {//增加参数
+        if(value != null) {
+            if(value instanceof String[]) {
+                params.put(name , (String[])value);
+            }else if(value instanceof String) {
+                params.put(name , new String[] {(String)value});
+            }else {
+                params.put(name , new String[] {String.valueOf(value)});
+            }
+        }
+    }
+
+}
