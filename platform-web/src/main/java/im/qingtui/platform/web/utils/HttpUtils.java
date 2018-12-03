e95927c875b00f216863a1c281f38f52c59011af
洞宾 <wei.b.yan@cisdi.com.cn>
2018-12-03 15:20:49 +0800

    添加一些方法

--- platform-web/src/main/java/im/qingtui/platform/web/utils/HttpUtils.java
+++ platform-web/src/main/java/im/qingtui/platform/web/utils/HttpUtils.java
@@ -1,21 +1,33 @@
 package im.qingtui.platform.web.utils;
 
 
+import com.alibaba.fastjson.JSONObject;
 import im.qingtui.platform.sensitive.SensitiveLevel;
 import im.qingtui.platform.sensitive.utils.SensitiveInfoUtils;
 import im.qingtui.platform.web.annotation.HttpElement;
+import im.qingtui.platform.web.wrapper.ParameterRequestWrapper;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.Cleanup;
+import lombok.val;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
 
 /**
  * 将http请求内容封装到map中
@@ -145,5 +157,83 @@ public class HttpUtils {
         return sb.toString();
     }
 
+
+    /**
+     * 向 httpResponse 中写入文件流
+     *
+     * @param fileName 文件名
+     * @param inputStream 输入流
+     */
+    public static void writeFileToResponse(HttpServletResponse response, String fileName, InputStream inputStream) throws IOException {
+        //设置响应头和客户端保存文件名
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("multipart/form-data");
+        setFileDownloadHeader(response, fileName);
+//        response.setHeader("Content-Disposition", "attachment;fileName=" + new String(fileName.getBytes("utf-8"), "iso-8859-1"));
+        //激活下载操作
+        @Cleanup OutputStream outputStream = response.getOutputStream();
+        //打开本地文件流
+        FileCopyUtils.copy(inputStream, outputStream);
+    }
+
+    /**
+     * 向 httpResponse 中写入返回
+     */
+    public static boolean writeResponse(ServletResponse response, Object result) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(JSONObject.toJSONString(result));
+        return false;
+    }
+
+    public static ParameterRequestWrapper getParameterRequestWrapper(HttpServletRequest request) {
+        ParameterRequestWrapper wrapper;
+        if (request instanceof DefaultMultipartHttpServletRequest) {
+            wrapper = (ParameterRequestWrapper) ((DefaultMultipartHttpServletRequest) request).getRequest();
+        } else {
+            wrapper = (ParameterRequestWrapper) request;
+        }
+        return wrapper;
+    }
+
+    public static boolean isUploadFileRequest(HttpServletRequest request) {
+        return request.getHeader("Content-Type") != null &&
+            request.getHeader("Content-Type").contains("multipart");
+    }
+
+    public static Map<String, String[]> getParameterMap(HttpServletRequest request) {
+        Map<String, String[]> parameterMap;
+        if (HttpUtils.isUploadFileRequest(request)) {
+            val fileRequest = (DefaultMultipartHttpServletRequest) request;
+            parameterMap = fileRequest.getParameterMap();
+        } else {
+            ParameterRequestWrapper wrapper = getParameterRequestWrapper(request);
+            parameterMap = wrapper.getParameterMap();
+        }
+        return parameterMap;
+    }
+
+    /**
+     * 浏览器下载文件时需要在服务端给出下载的文件名，当文件名是ASCII字符时没有问题当文件名有非ASCII字符时就有可能出现乱码 最终设置的response header是这样: Content-Disposition: attachment; filename="encoded_text";
+     * filename*=utf-8''encoded_text 其中encoded_text是经过RFC 3986的“百分号URL编码”规则处理过的文件名
+     */
+    public static void setFileDownloadHeader(HttpServletResponse response, String filename) {
+        String headerValue = "attachment;";
+        headerValue += " filename=\"" + encodeURIComponent(filename) + "\";";
+        headerValue += " filename*=utf-8''" + encodeURIComponent(filename);
+        response.setHeader("Content-Disposition", headerValue);
+    }
+
+    /**
+     * 符合 RFC 3986 标准的“百分号URL编码”在这个方法里，空格会被编码成%20，而不是+和浏览器的encodeURIComponent行为一致
+     */
+    public static String encodeURIComponent(String value) {
+        try {
+            return URLEncoder.encode(value, "UTF-8").replaceAll("\\+", "%20");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
