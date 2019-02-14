8502c010871cbf6c808cc3baea46cac589e408e4
洞宾 <wei.b.yan@cisdi.com.cn>
2019-02-14 11:45:01 +0800

    code不能为null了

--- platform-vo/src/main/java/im/qingtui/platform/web/vo/BaseVo.java
+++ platform-vo/src/main/java/im/qingtui/platform/web/vo/BaseVo.java
@@ -22,9 +22,9 @@ public class BaseVo implements Serializable {
 
 
     private static final long serialVersionUID = 1L;
-    private static final long DEFAULT_SUCCESS_CODE = 0L;
+    private static final Long DEFAULT_SUCCESS_CODE = 0L;
 
-    private long code = DEFAULT_SUCCESS_CODE;
+    private Long code = DEFAULT_SUCCESS_CODE;
 
     private String message;
 
@@ -39,6 +39,10 @@ public class BaseVo implements Serializable {
 
     public static BaseVo defaultSuccess() {
         return DEFAULT_SUCCESS;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
     }
 
     public void setCode(long code) {
